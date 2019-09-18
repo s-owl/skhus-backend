b0d0576ee01418ac83847d8c282e835f4b3bd356
tpdns90321 <tpdns9032100@gmail.com>
2019-09-18 15:39:09 +0900

    [hotfix/browser-timeout] 주기적으로 활당 컨텍스트를 종료한다.

--- browser/browser.go
+++ browser/browser.go
@@ -2,6 +2,8 @@ package browser
 
 import (
 	"log"
+	"sync"
+	"time"
 	"context"
 
 	"github.com/s-owl/skhus-backend/consts"
@@ -13,26 +15,65 @@ import (
 type browser struct {
 	allocCtx context.Context
 	cancelCtx context.CancelFunc
+	*sync.RWMutex
 }
 
-var singletone *browser
+func (brow *browser) isOk() bool {
+	return brow.allocCtx != nil
+}
 
-func GetBrowser() *browser {
-	if singletone == nil {
+func (brow *browser) init() {
+	brow.Lock()
+	defer brow.Unlock()
+	if !brow.isOk() {
 		// Options for custom user agent
 		opts := append(chromedp.DefaultExecAllocatorOptions[:],
 			chromedp.UserAgent(consts.UserAgentIE))
-
 		// Create contexts
-		allocCtx, cancelCtx := chromedp.NewExecAllocator(context.Background(), opts...)
-		singletone = &browser {
-			allocCtx: allocCtx,
-			cancelCtx: cancelCtx,
-		}
+		brow.allocCtx, brow.cancelCtx =
+			chromedp.NewExecAllocator(context.Background(), opts...)
+		//특정 시간 후에 브라우저를 닫는다
+		go func() {
+			<-time.After(1*time.Minute)
+			brow.close()
+		}()
+	}
+}
+
+func (brow *browser) close() {
+	brow.Lock()
+	defer brow.Unlock()
+	brow.cancelCtx()
+	brow.allocCtx = nil
+}
+
+var singletone *browser = &browser{
+	RWMutex: &sync.RWMutex{},
+}
+
+// GetBrowser 브라우저를 가져온다.
+func GetBrowser() *browser {
+	// 초기화되지 않을 때 초기화
+	if !singletone.isOk() {
+		singletone.init()
 	}
 	return singletone
 }
 
-func (brow *browser)NewContext() (context.Context, context.CancelFunc) {
-	return chromedp.NewContext(brow.allocCtx, chromedp.WithLogf(log.Printf))
+// NewContext 컨텍스트 생성
+func (brow *browser)NewContext() (ctx context.Context, cf context.CancelFunc) {
+	brow.RLock()
+	// 최악의 경우의 수인 브라우저 닫히고 컨텍스트를 생성할 때
+	if !brow.isOk() {
+		brow.RUnlock()
+		brow.init()
+		brow.RLock()
+	}
+	ctx, cf = chromedp.NewContext(brow.allocCtx, chromedp.WithLogf(log.Printf))
+	// 컨텍스트 종료시 뮤텍스 해제
+	go func() {
+		<-ctx.Done()
+		brow.RUnlock()
+	}()
+	return
 }
