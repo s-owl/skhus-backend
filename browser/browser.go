34f6ca4df07c871aa01973f7a92c14e1b25c7bc3
tpdns90321 <tpdns9032100@gmail.com>
2019-10-02 11:44:35 +0900

    [hotfix/browser-timeout] 코드 리팩토링

--- browser/browser.go
+++ browser/browser.go
@@ -11,11 +11,13 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+const BROWSER_TIMEOUT = 1
+
 // chromedp의 ExecAllocator를 singletone으로 관리하는 구조체(접근제어를 위해 소문자)
 type browser struct {
-	allocCtx context.Context
+	allocCtx  context.Context
 	cancelCtx context.CancelFunc
-	*sync.RWMutex
+	mutex     *sync.RWMutex
 }
 
 func (brow *browser) isOk() bool {
@@ -23,57 +25,49 @@ func (brow *browser) isOk() bool {
 }
 
 func (brow *browser) init() {
-	brow.Lock()
-	defer brow.Unlock()
-	if !brow.isOk() {
-		// Options for custom user agent
-		opts := append(chromedp.DefaultExecAllocatorOptions[:],
-			chromedp.UserAgent(consts.UserAgentIE))
-		// Create contexts
-		brow.allocCtx, brow.cancelCtx =
-			chromedp.NewExecAllocator(context.Background(), opts...)
-		//특정 시간 후에 브라우저를 닫는다
-		go func() {
-			<-time.After(1*time.Minute)
-			brow.close()
-		}()
-	}
+	// Options for custom user agent
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.UserAgent(consts.UserAgentIE))
+	// Create contexts
+	brow.allocCtx, brow.cancelCtx =
+		chromedp.NewExecAllocator(context.Background(), opts...)
+	//특정 시간 후에 브라우저를 닫는다
+	go func() {
+		<-time.After(BROWSER_TIMEOUT*time.Minute)
+		brow.reopen()
+	}()
 }
 
-func (brow *browser) close() {
-	brow.Lock()
-	defer brow.Unlock()
+func (brow *browser) reopen() {
+	brow.mutex.Lock()
+	defer brow.mutex.Unlock()
 	brow.cancelCtx()
-	brow.allocCtx = nil
+	brow.init()
 }
 
 var singletone *browser = &browser{
-	RWMutex: &sync.RWMutex{},
+	nil, nil, &sync.RWMutex{},
 }
 
 // GetBrowser 브라우저를 가져온다.
 func GetBrowser() *browser {
 	// 초기화되지 않을 때 초기화
 	if !singletone.isOk() {
+		singletone.mutex.Lock()
 		singletone.init()
+		singletone.mutex.Unlock()
 	}
 	return singletone
 }
 
 // NewContext 컨텍스트 생성
 func (brow *browser)NewContext() (ctx context.Context, cf context.CancelFunc) {
-	brow.RLock()
-	// 최악의 경우의 수인 브라우저 닫히고 컨텍스트를 생성할 때
-	if !brow.isOk() {
-		brow.RUnlock()
-		brow.init()
-		brow.RLock()
-	}
+	brow.mutex.RLock()
 	ctx, cf = chromedp.NewContext(brow.allocCtx, chromedp.WithLogf(log.Printf))
 	// 컨텍스트 종료시 뮤텍스 해제
 	go func() {
 		<-ctx.Done()
-		brow.RUnlock()
+		brow.mutex.RUnlock()
 	}()
 	return
 }
