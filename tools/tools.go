57ea78c14be346bfe6cad4820d92c52fc005d0ff
tpdns90321 <tpdns9032100@gmail.com>
2020-01-23 15:26:53 +0900

    [feature/web-cookie] 호환성을 유지한 채 쿠키 헤더를 받는다.
    실제 브라우저에서 쿠키를 쉽게 전송하기 위해 fetch의 credentials 옵션을
    사용하는데 이 옵션은 Cookie 헤더로만 전송해서 SKHUS API는 Credential
    헤더만 사용해서 Cookie 헤더도 같이 받을 수 있게 수정한다.

--- tools/tools.go
+++ tools/tools.go
@@ -2,11 +2,11 @@ package tools
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/s-owl/skhus-backend/consts"
@@ -37,34 +37,47 @@ func EucKrReaderToUtf8Reader(body io.Reader) io.Reader {
 
 func CredentialOldCheckMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		credential := c.GetHeader("Credential")
+		var cookies []*http.Cookie
+		var err error
+		var credential string
+
+		for _, headerName := range []string{"Cookie", "Credential"} {
+			credential = c.GetHeader(headerName)
+			if credential == "" {
+				fmt.Println("empty ", headerName)
+				continue
+			}
+			for _, item := range []string{"ASP.NET_SessionId", ".AuthCookie", "UniCookie"} {
+				if !strings.Contains(credential, item) {
+					fmt.Println("not full cookie: ", item)
+					c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
+					c.Abort()
+					return
+				}
+			}
+			if len(strings.Split(credential, ";")) < 5 {
+				fmt.Println("cookie number wrong")
+				c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
+				c.Abort()
+				return
+			}
+			cookies, err = ConvertToCookies(credential)
+			if err != nil {
+				fmt.Println("Wrong Cookie")
+				c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
+				c.Abort()
+				return
+			}
+			break
+		}
+
 		if credential == "" {
 			fmt.Println("empty credential")
 			c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
 			c.Abort()
 			return
 		}
-		for _, item := range []string{"ASP.NET_SessionId", ".AuthCookie", "UniCookie", "KIS"} {
-			if !strings.Contains(credential, item) {
-				fmt.Println("not full cookie")
-				c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
-				c.Abort()
-				return
-			}
-		}
-		if len(strings.Split(credential, ";")) != 5 {
-			fmt.Println("cookie number wrong")
-			c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
-			c.Abort()
-			return
-		}
-		cookies, err := ConvertToCookies(credential)
-		if err != nil {
-			fmt.Println("Wrong Cookie")
-			c.String(http.StatusBadRequest, consts.CredentialMalformedMsg)
-			c.Abort()
-			return
-		}
+
 		c.Set("CredentialOldCookies", cookies)
 		c.Set("CredentialOld", credential)
 		c.Next()
