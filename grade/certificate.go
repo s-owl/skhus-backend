db28448d05d4b37f94fa9a61089465e7ae4b11d4
tpdns90321 <tpdns9032100@gmail.com>
2020-05-20 15:41:54 +0900

    [feature/docs] grade/certificate.go에 설명 추가

--- grade/certificate.go
+++ grade/certificate.go
@@ -1,7 +1,6 @@
 package grade
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -12,8 +11,14 @@ import (
 	"github.com/s-owl/skhus-backend/tools"
 )
 
+// Forest에서 학점을 가져온다.
+/*
+	{
+	}
+*/
 func GetGradeCertificate(c *gin.Context) {
-	targetURL := fmt.Sprintf("%s/GATE/SAM/SCORE/S/SSJS06S.ASPX?&maincd=O&systemcd=S&seq=1", consts.ForestURL)
+	// forest에 접근해서 html을 받아온다.
+	targetURL := consts.ForestURL + "/GATE/SAM/SCORE/S/SSJS06S.ASPX?&maincd=O&systemcd=S&seq=1"
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", targetURL, nil)
 	req.Header.Add("Cookie", c.MustGet("CredentialOld").(string))
@@ -25,14 +30,18 @@ func GetGradeCertificate(c *gin.Context) {
 	}
 	defer res.Body.Close()
 
+	// 받아온 html을 goquery로 분석한다.
 	doc, err := goquery.NewDocumentFromReader(tools.EucKrReaderToUtf8Reader(res.Body))
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	// 사용자의 정보를 담을 변수를 초기화
 	userinfo := []gin.H{}
+	// 사용자의 정보가 담긴 테이블을 찾는다.
 	userdata := doc.Find("#Table3 > tbody > tr > td")
+	// 테이블을 순회해서 userinfo에 담는다.
 	userdata.Each(func(i int, item *goquery.Selection) {
 		if i%2 != 0 {
 			userinfo = append(userinfo, gin.H{
@@ -42,8 +51,11 @@ func GetGradeCertificate(c *gin.Context) {
 		}
 	})
 
+	// 사용자의 학점을 담을 변수를 초기화
 	details := []gin.H{}
+	// 학점이 존재하는 테이블을 찾아 순회한다.
 	doc.Find("table#dgList > tbody > tr").Each(func(i int, item *goquery.Selection) {
+		// 넌도, 학기, 교유번호, 과목, 종류, 학점, 등급 순으로 들어온다.
 		details = append(details, gin.H{
 			"year":     item.Children().Eq(0).Text(),
 			"semester": item.Children().Eq(1).Text(),
@@ -55,7 +67,9 @@ func GetGradeCertificate(c *gin.Context) {
 		})
 	})
 
+	// 요약 부분을 저장할 변수를 초기화한다.
 	summary := []gin.H{}
+	// 요약 정보를 찾고 순회해서 저장한다.
 	summaryData := doc.Find("table#Table2 > tbody > tr")
 	for i := 0; i < 17; i++ {
 		summary = append(summary, gin.H{
@@ -64,8 +78,10 @@ func GetGradeCertificate(c *gin.Context) {
 		})
 	}
 
+	// 인증 날짜를 찾아 저장한다.
 	certDate := doc.Find("#lblDt").Text()
 
+	// 결과값을 전송한다.
 	c.JSON(http.StatusOK, gin.H{
 		"userinfo": userinfo,
 		"details":  details,
